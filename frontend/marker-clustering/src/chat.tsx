9fd979c93742d81af665b0a1d0b3b75fa14bc488
HenrikMoe <henrikmoe29@gmail.com>
2024-02-23 16:47:02 -0700

    dm sending render, stripe key, payment charge tracking on users, withdrawl route, balalnce retreiveal route, jpgpng heic check, calendar, userrating, transactions bdd, branding,  making 9 more parks ready to integrate,  production

--- frontend/marker-clustering/src/chat.tsx
+++ frontend/marker-clustering/src/chat.tsx
@@ -53,7 +53,6 @@ const Chat: React.FC<ChatProps> = ({ dogParkName, username, openUser }) => {
     if (newMessage.trim() !== '') {
       receiveMessage({ user: selectedDMUser, message: newMessage, timestamp: new Date().toISOString() });
 
-
       try {
         const targetEndpoint = activeTab === 'park' ? `sendmessage/${dogParkName}` : `dms/${username}`;
         await fetch(`http://localhost:3029/${targetEndpoint}`, {
